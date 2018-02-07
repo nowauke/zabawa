4591b63d52a744a82ed1181e254a823e4f9c0ce8
Lenovo <Lenovo@DESKTOP-SMKDTBB>
2018-02-07 10:50:06 +0100

    oferta nowa

---
+++ Oferta.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$conn = mysqli_connect('nowk.pl', 'nowk', 'wq5wceyC', 'nowk_loguj', '3306');
+if (!$conn) {
+    die('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+mysqli_query($conn, 'SET NAMES \'utf8\'');
+
+echo '<table>';
+echo '<tr>';
+echo '<th>nazwa</th>';
+echo '<th>cena</th>';
+echo '<th>zdjęcie</th>';
+echo '</tr>';
+$result = mysqli_query($conn, 'SELECT nazwa, cena, `zdjęcie` FROM OFERTA');
+while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
+    echo '<tr>';
+    echo '<td>' . $row['nazwa'] . '</td>';
+    echo '<td>' . $row['cena'] . '</td>';
+    echo '<td>' . $row['zdjęcie'] . '</td>';
+    echo '</tr>';
+}
+mysqli_free_result($result);
+echo '</table>';
+mysqli_close($conn);
+
+
+
+
+?>
