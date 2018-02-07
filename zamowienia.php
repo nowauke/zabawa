df1e6300169c5e4ced4dff3426201b5813e4ff94
NOWAUKE <NOWAUKE@NOWAUKE-KOMPUTE>
2018-02-07 10:54:24 +0100

    zmiany 1

---
+++ zamowienia.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>Zamówienia</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>
+            <form action="zamowienia.php" method="post">
+                Imię: <input type="text" name="name"><br>
+                Nazwisko: <input type="text" name="surname"><br>
+                Miejscowośc: <input type="text" name="city"><br>
+                Ulica: <input type="text" name="street"><br>
+                
+            </form>
+        </div>
+        
+        
+        
+        
+        
+    </body>
+</html>
